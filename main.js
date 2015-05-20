2dc69182c915d5445e416c6764685882e8991d4c
Namita Malik <namita.malik@paytm.com>
2015-05-20 16:35:36 +0530

    Adding twitter bootstrap

--- main.js
+++ main.js
@@ -6,7 +6,10 @@ require.config({
     paths: {
         'angular': './node_modules/angular/angular.min',
         'angular-route': './node_modules/angular-route/angular-route.min',
-        'angularAMD': './node_modules/angularAMD/dist/angularAMD.min'
+        'angularAMD': './node_modules/angularAMD/dist/angularAMD.min',
+        'jquery': './jquery-1.11.3.min',
+        'bootstrap': './js/bootstrap.min'
+
     },
     shim: {
         'angularAMD': ['angular'], 'angular-route':['angular']
