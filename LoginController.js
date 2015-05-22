ca92ee502fb6724af6e9124dfb6274742346f522
Namita Malik <namita.malik@paytm.com>
2015-05-22 18:26:07 +0530

    Post request

--- LoginController.js
+++ LoginController.js
@@ -3,9 +3,20 @@
  */
 (function (ng) {
     var loginApp = ng.module('loginApp', []);
-    loginApp.controller('LoginController', ['$scope', function ($scope) {
+    loginApp.controller('LoginController', ['$scope','$http', function ($scope,$http) {
         $scope.showSignUpModal = function () {
             ng.element('#myModal').modal('show');
         };
+        $scope.registerUser = function () {
+            $http.post('/login', {test: "test"}).
+                success(function (data, status, headers, config) {
+                    // this callback will be called asynchronously
+                    // when the response is available
+                }).
+                error(function (data, status, headers, config) {
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+                });
+        }
     }]);
 })(angular);
--- main.js
+++ main.js
@@ -4,9 +4,9 @@
 require.config({
     baseUrl: "",
     paths: {
-        'angular': './node_modules/angular/angular.min',
-        'angular-route': './node_modules/angular-route/angular-route.min',
-        'angularAMD': './node_modules/angularAMD/dist/angularAMD.min',
+        'angular': 'node_modules/angular/angular.min',
+        'angular-route': 'node_modules/angular-route/angular-route.min',
+        'angularAMD': 'node_modules/angularAMD/dist/angularAMD.min',
         'jquery': './jquery-1.11.3.min',
         'bootstrap': './js/bootstrap.min'
 
